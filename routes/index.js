9142b9cd58a035ad00dc774726096b88ba127bf7
roybass <roybass@gmail.com>
2016-09-02 02:19:22 +0300

    Adding Swagger UI

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/swagger-ui/index.html');
 });
 
 module.exports = router;
